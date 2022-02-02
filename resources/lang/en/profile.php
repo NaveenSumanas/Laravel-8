6a393df54b52d690e462fc9ca23a58059e2509bf
Gulam Ahamed <gulamahamed@arkinfotec.com>
2022-02-02 17:34:07 +0530

    Pluralization concepts

--- resources/lang/en/profile.php
+++ resources/lang/en/profile.php
@@ -6,8 +6,7 @@ return [
     'about' => 'about',
     'list' => 'list',
     'contact' => 'contact',
-    'apples' => 'There is one apple|There are many apples',
     'apples' => '{0} There are none|[1,19] There are some|[20,*] There are many',
-    //"There is one apple|There are many apples": "Hay una manzana|Hay muchas manzanas",
+    'minutes_ago' => '{1} :value minute ago|[2,*] :value minutes ago',
 
 ];
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,3 +7,9 @@
 <a href="">{{__('profile.list')}}</a>
 <a href="">{{__('profile.about')}}</a>
 <a href="">{{__('profile.contact')}}</a>
+<br>
+
+{{ trans_choice('There is one apple|There are many apples', 2) }}<br>
+{{ trans_choice('profile.apples',21) }}<br><br>
+{{ trans_choice('profile.minutes_ago', 1, ['value' => 5]) }}
+<br>
